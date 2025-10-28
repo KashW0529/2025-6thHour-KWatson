9fc5bf64de1f474a53ebf8eaa6e4d9a66462a48c
Kash Watson <kw125079@student.wpsok.org>
2025-10-28 13:28:19 -0500

    Finished HW10 and HW11

---
+++ HW10.py
@@ -0,0 +1,30 @@
+#Name: Kash
+#Class: 6th Hour
+#Assignment: HW10
+
+#1. Print Hello World!
+print("Hello World!")
+#2. Create three different boolean variables named wifi, login, and admin.
+wifi = False
+login = True
+admin = True
+#3. Create a separate integer variable that denotes the number of times
+#someone with admin credentials has logged in.
+admin_login_count = 41
+#4. Create a nested if statement that checks to see if wifi is true,
+#login is true, and admin is true. If they are all true, print a
+#welcome message and increase the integer variable by one. If one of them
+#is false, print an error message telling them which one they are "missing".
+
+if wifi == True:
+    if login == True:
+        if admin == True:
+            print("Welcome admin!")
+            admin_login_count += 1
+            print("Admin Login:", admin_login_count)
+        else:
+            print("Access Denied")
+    else:
+        print("Incorrect Password")
+else:
+    print("No Internet 🦖")
---
+++ HW11.py
@@ -0,0 +1,35 @@
+#Name: Kash
+#Class: 6th Hour
+#Assignment: HW11
+
+import random
+
+#1. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
+
+#import random library
+
+#Create a temperature variable, give it a value (random from 1 to 30).
+
+#Make an if statement to see if temperature variable is above 20
+#   - If yes, print it's hot
+#   - If no, bring it to next if statement
+
+#Make an if statement to see if temperature variable is above 10
+#   - If yes, print it's mild
+#   - If no, print it's cold
+
+#Print "Thank you!" and end the code
+
+temp = random.randint(1,30)
+print(temp)
+
+if temp > 20:
+    print("It's hot!")
+elif temp > 10:
+    print("It's mild!")
+else:
+    print("It's cold!")
+
+
