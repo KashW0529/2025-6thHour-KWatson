8b015857adffabd39a0ada37e1a54fa66758dd4c
Kash Watson <kw125079@student.wpsok.org>
2026-01-08 13:45:58 -0600

    HW15

---
+++ HW15.py
@@ -0,0 +1,35 @@
+#Name: Kash
+#Class: 6th Hour
+#Assignment: HW15
+
+#1. import the "random" library
+import random
+#2. print "Hello World!"
+print('hello world')
+#3. Create three variables named a, b, and c, and allow the user to input an integer for each.
+a = int(input('enter a integer for a'))
+b = int(input('enter a integer for b'))
+c = int(input('enter a integer for c'))
+#4. Add a and b together, then divide the sum by c. Print the result.
+c = a + b
+print(c)
+#5. Round the result from #3 up or down, and then determine if it is even or odd.
+c = print(round(3.14))
+#6. Create a list of five different random integers between 1 and 10.
+int_list = [1,2,3,4,5]
+#7. Print the 4th number in the list.
+print(int_list[4])
+#8. Append another integer to the end of the list, also random from 1 to 10.
+int_list.append(6)
+#9. Sort the list from lowest to highest and then print the 4th number in the list again.
+int_list.sort()
+print(int_list)
+#10. Create a while loop that starts at 1, prints i and then adds i to itself until it is greater than 100.
+
+#11. Create a list containing the names of five other students in the classroom.
+
+#12. Create a for loop that individually prints out the names of each student in the list.
+
+#13. Create a for loop that counts from 1 to 100, but ends early if the number is a multiple of 10.
+
+#14. Free space. Do something creative. :)
